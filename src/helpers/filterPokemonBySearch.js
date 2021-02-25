cc64ac35cf65753b202a261cf18ae72dcde5640f
TLex <tlex1@web.de>
2021-02-25 23:53:41 +0100

    Finished simple version

--- src/helpers/filterPokemonBySearch.js
+++ src/helpers/filterPokemonBySearch.js
@@ -1,6 +1,5 @@
 const filterPokemonBySearch = (allPokemon, searchText) =>
   allPokemon.filter((pokemon) => pokemon.name.includes(searchText.toLowerCase()))
-  .slice(0,20)
 ;
 
 export default filterPokemonBySearch
